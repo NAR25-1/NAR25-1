bae6779a57eaa20f916f02f75f7459f17148c0b2
GIGABYTE <nathanaelmoses>
2024-12-24 17:57:40 +0700

    moses

--- MySQL/trainees.sql
+++ MySQL/trainees.sql
@@ -2,24 +2,25 @@ CREATE TABLE trainee(
 	trainee_number CHAR(4) PRIMARY KEY,
     trainee_nama VARCHAR(251),
     trainee_binusian CHAR(3),
-    trainee_major VARCHAR(251)
+    trainee_major VARCHAR(251),
+    trainee_pass VARCHAR(251)    
 )
 
-INSERT INTO trainee(trainee_number,trainee_nama,trainee_binusian,trainee_major) VALUES
-('T186', 'Marcell Sutiono', 'GAT', 'B27'),
-('T191', 'Jason Tanuwijaya Tariono', 'CS', 'B27'),
-('T192', 'Rafael Febrian', 'CS', 'B27'),
-('T195', 'Juan Verron Anggani', 'CS', 'B27'),
-('T207', 'Ivy Nikita Malvin', 'MIT', 'B28'),
-('T212', 'Sisilia Annabel Aileen', 'CS', 'B28'),
-('T213', 'Kenneth Maximillian Pranata', 'CS', 'B28'),
-('T217', 'Nathanael Moses', 'DS', 'B28'),
-('T228', 'Jonathan Christian Wongso', 'CS', 'B28'),
-('T230', 'James Alexander Moses Kartolo', 'CYBER', 'B28'),
-('T237', 'Gavin Justine', 'CYBER', 'B28'),
-('T241', 'Vincentius Richardo Putra', 'MIT', 'B28'),
-('T252', 'Evorius Valens Taruna', 'CS&M', 'B28'),
-('T297', 'Sean Spencer', 'CS', 'B28'),
-('T312', 'Daniel Regis Febrianto', 'CS', 'B28'),
-('T330', 'Alin Lorensia', 'CS', 'B28'),
-('T355', 'Weneville', 'CS&M', 'B28');
+INSERT INTO trainee(trainee_number,trainee_pass,trainee_nama,trainee_binusian,trainee_major,trainee_pass) VALUES
+('T186', 'Marcell Sutiono', 'GAT', 'B27', 'T186Marcell SutionoGATB27'),
+('T191', 'Jason Tanuwijaya Tariono', 'CS', 'B27', 'T191Jason Tanuwijaya TarionoCSB27'),
+('T192', 'Rafael Febrian', 'CS', 'B27', 'T192Rafael FebrianCSB27'),
+('T195', 'Juan Verron Anggani', 'CS', 'B27', 'T195Juan Verron AngganiCSB27'),
+('T207', 'Ivy Nikita Malvin', 'MIT', 'B28', 'T207Ivy Nikita MalvinMITB28'),
+('T212', 'Sisilia Annabel Aileen', 'CS', 'B28', 'T212Sisilia Annabel AileenCSB28'),
+('T213', 'Kenneth Maximillian Pranata', 'CS', 'B28', 'T213Kenneth Maximillian PranataCSB28'),
+('T217', 'Nathanael Moses', 'DS', 'B28', 'T217Nathanael MosesDSB28'),
+('T228', 'Jonathan Christian Wongso', 'CS', 'B28', 'T228Jonathan Christian WongsoCSB28'),
+('T230', 'James Alexander Moses Kartolo', 'CYBER', 'B28', 'T230James Alexander Moses KartoloCYBERB28'),
+('T237', 'Gavin Justine', 'CYBER', 'B28', 'T237Gavin JustineCYBERB28'),
+('T241', 'Vincentius Richardo Putra', 'MIT', 'B28', 'T241Vincentius Richardo PutraMITB28'),
+('T252', 'Evorius Valens Taruna', 'CS&M', 'B28', 'T252Evorius Valens TarunaCS&MB28'),
+('T297', 'Sean Spencer', 'CS', 'B28', 'T297Sean SpencerCSB28'),
+('T312', 'Daniel Regis Febrianto', 'CS', 'B28', 'T312Daniel Regis FebriantoCSB28'),
+('T330', 'Alin Lorensia', 'CS', 'B28', 'T330Alin LorensiaCSB28'),
+('T355', 'Weneville', 'CS&M', 'B28', 'T355WenevilleCS&MB28');
--- app.py
+++ app.py
@@ -12,14 +12,16 @@ def query():
     results = None  
     if request.method == 'POST':
         trainee_numb = request.form.get('trainee_number')
+        trainee_pass = request.form.get('trainee_pass')
         connection = get_db_connection()
         if connection is None:
             return "Failed to connect to the database!"
         try:
             with connection.cursor() as cursor:
-                query = "SELECT * FROM trainee WHERE trainee_number = %s"
-                cursor.execute(query, (trainee_numb,))
+                query = "SELECT * FROM trainee WHERE trainee_number = %s AND trainee_pass = %s"
+                cursor.execute(query, (trainee_numb,trainee_pass))
                 results = cursor.fetchall() 
+                print(results   )
         finally:
             connection.close()
     return render_template("test.html", results=results)
